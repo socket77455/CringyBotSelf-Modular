5ebb3f8e651e490da532dbc63d113a44c2704bfc
Adam <adam.aharony@icloud.com>
2017-05-21 22:52:51 +0300

    added terminal in the help command

--- commands/help.js
+++ commands/help.js
@@ -15,26 +15,27 @@ exports.run = (client, message, args) => {
             color: 0x008AF3,
             title: "CringyBot Selfbot edition Help",
             description: `
-		    **${config.prefix}info** - shows info about the bot.
-		    **${config.prefix}github** - sends link to github.
-		    **${config.prefix}help** - displays this message.
-		    **${config.prefix}google** - searches Google.
-		    **${config.prefix}spam** <number>  - spams a message. **USE WITH CAUTION!**
-		    **${config.prefix}nick** - changes your nickname on this server.
-		    **${config.prefix}game** - changes the game you're playing.
-		    **${config.prefix}stream** - changes the streaming status.
-		    **${config.prefix}embed** - puts messages in an embed with your user name and profile pic.
-		    **${config.prefix}smallembed** - embeds stuff in a smaller form.
-		    **${config.prefix}status** - displays info about you.
-		    **${config.prefix}purge** <number> - purges a number of your messages.
-		    **${config.prefix}eval** - evaluates and executes JS code that's it.
-		    **${config.prefix}hammer** - sends the discord ban hammer gif.
-		    **${config.prefix}lewis** - shows a really good pic of ya boi Lew.
-		    **${config.prefix}rickroll** - sends a link to the song.
-		    **${config.prefix}ping** - really? do you need a description for that?
-		    **${config.prefix}shutdown** - shuts the selfbot down.
-		    **${config.prefix}reload** <command> - reloads a command.
-		    **${config.prefix}time** - shows the server's time.`,
+		            **${config.prefix}info** - shows info about the bot.
+		            **${config.prefix}github** - sends link to github.
+		            **${config.prefix}help** - displays this message.
+		            **${config.prefix}google** - searches Google.
+		            **${config.prefix}spam** <number>  - spams a message. **USE WITH CAUTION!**
+		            **${config.prefix}nick** - changes your nickname on this server.
+		            **${config.prefix}game** - changes the game you're playing.
+		            **${config.prefix}stream** - changes the streaming status.
+		            **${config.prefix}embed** - puts messages in an embed with your user name and profile pic.
+		            **${config.prefix}smallembed** - embeds stuff in a smaller form.
+		            **${config.prefix}status** - displays info about you.
+			    **${config.prefix}purge** <number> - purges a number of your messages.
+			    **${config.prefix}terminal** <command> - runs a terminal command.
+		            **${config.prefix}eval** - evaluates and executes JS code that's it.
+		            **${config.prefix}hammer** - sends the discord ban hammer gif.
+		            **${config.prefix}lewis** - shows a really good pic of ya boi Lew.
+		            **${config.prefix}rickroll** - sends a link to the song.
+		            **${config.prefix}ping** - really? do you need a description for that?
+		            **${config.prefix}shutdown** - shuts the selfbot down.
+		            **${config.prefix}reload** <command> - reloads a command.
+		            **${config.prefix}time** - shows the server's time.`,
             timestamp: new Date(),
             footer: {
                 text: 'CringyBot Selfbot edition',
